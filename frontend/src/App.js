23e6e773762b95bedfebdd1e7c32389d2de09b06
nico.noell <nico.noell@advancis.de>
2024-11-23 20:26:03 +0100

    Added Tables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,67 @@ function App() {
   const [scenarioId, setScenarioId] = useState(null);
   const [scenarioLoaded, setScenarioLoaded] = useState(false);
 
+  const fetchData = async (sc) => {
+    setLoading(true);
+    const res = await fetch(
+      `http://127.0.0.1:5000/api/getData?scenarioId=${sc}`
+    );
+    if (!res.ok) throw new Error("Fehler beim Abrufen der Daten");
+    const data = await res.json();
+    setData(data);
+    setLoading(false);
+  };
+
+  const renderData = () => (
+    <div>
+    <h2>Vehicles</h2>
+    <table>
+        <tr>
+            <th>Position</th>
+            <th>Speed</th>
+            <th>CustomerId</th>
+            <th>Remaining<br/>TravelTime</th>
+            <th>Distance<br/>Travelled</th>
+            <th>activeTime</th>
+            <th>Trips</th>
+            <th>Id</th>
+        </tr>
+    {data?.vehicles.map((item) => (
+    //   <div
+    //     key={item.id}
+    //     >
+        <tr class={item.isAvailable ? "available" : "occupied"}>
+            <td>{item.coordX}, {item.coordY}</td>
+            <td>{item.vehicleSpeed}</td>
+            <td class="idTabledata">{item.customerId}</td>
+            <td>{item.remainingTravelTime}</td>
+            <td>{item.distanceTravelled}</td>
+            <td>{item.activeTime}</td>
+            <td>{item.numberOfTrips}</td>
+            <td class="idTabledata">{item.id}</td>
+        </tr>
+    ))}
+    </table>
+    
+    <h2>Customers</h2>
+    <table>
+        <tr>
+            <th>Position</th>
+            <th>Destination</th>
+            <th>Awaiting Service</th>
+            <th>Id</th>
+        </tr>
+    {data?.customers.map((item) => (
+        <tr class={item.awaitingService ? "occupied" : "available"}>
+            <td>{item.coordX}, {item.coordY}</td>
+            <td>{item.destinationX}, {item.destinationY}</td>
+            <td>{item.customerId}</td>
+            <td class="idTabledata">{item.id}</td>
+        </tr>
+    ))}
+    </table>
+    </div>);
+
   return (
     <div className="App">
       <h1>Taxi-Overview</h1>
