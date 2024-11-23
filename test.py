243c301e78246aec641e8adca8c6ff5b369d24ef
nico.noell <nico.noell@advancis.de>
2024-11-23 18:55:00 +0100

    Add some stuff

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from backend.TelcAPI import Backend, Runner
 from backend.Wrapper.Scenario import Scenario
+from backend.Wrapper.VehicleUpdate import VehicleUpdate
+from backend.Wrapper.UpdateScenario import UpdateScenario
 import json
 
-scenario: Scenario = Backend.getScenario("973dc6f2-2cde-4872-9689-ac7f968790c0")
+scenario: Scenario = Backend.getScenario("268bdbc6-1e37-4392-ab63-8b4b557bd0b5")
+Runner.initScenario(scenario)
+Runner.launchScenario(scenarioId=scenario.id, speed=40)
+vehicles = Runner.getScenario(scenario)
 
+vehicleUpdate = VehicleUpdate("7804ce9a-d76f-45c2-bd75-5a321d7f0614", scenario.customers[0].id)
+updateScenario = UpdateScenario([vehicleUpdate])
+Runner.updateScenario(scenario.id, updateScenario)
 print(scenario.json())
