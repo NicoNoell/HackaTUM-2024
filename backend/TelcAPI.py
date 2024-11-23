443d7e98b5f173d6e3ffece6984d8966c6ba1889
Felix Wu <felix@dot9.co>
2024-11-23 12:16:32 +0100

    modules

--- backend/TelcAPI.py
+++ backend/TelcAPI.py
@@ -1,42 +1,54 @@
 import requests
-from Wrapper.Scenario import Scenario
-from Wrapper.Customer import Customer
-from Wrapper.Vehicle import Vehicle
+from backend.Wrapper.Scenario import Scenario
+from backend.Wrapper.Customer import Customer
+from backend.Wrapper.Vehicle import Vehicle
 
 # NOTE: Um ein Scenario im Runner zu nutzen, muss es zuerst mit get_scenario vom Backend abgefragt bzw.
 #       anderweitig erstellt werden und dann mit Runner.init_scenario dem Runner übergeben werden.
 
 PAYLOAD_HEADER = {"Content-Type": "application/json"}
 
-class Runner():
-    def getScenario(scenarioId:str) -> Scenario:
+
+class Runner:
+    def getScenario(scenarioId: str) -> Scenario:
         try:
             return Scenario(
-                requests.get(f"http://localhost:8090/Scenarios/get_scenario/{scenarioId}").json()
+                requests.get(
+                    f"http://localhost:8090/Scenarios/get_scenario/{scenarioId}"
+                ).json()
             )
         except:
             print("Response ist None oder ungültig")
 
-    def initScenario(scenario:Scenario) -> None:
+    def initScenario(scenario: Scenario) -> None:
         try:
-            requests.post(f"http://localhost:8090/Scenarios/initialize_scenario", json=scenario.json(), headers=PAYLOAD_HEADER).json()
+            requests.post(
+                f"http://localhost:8090/Scenarios/initialize_scenario",
+                json=scenario.json(),
+                headers=PAYLOAD_HEADER,
+            ).json()
         except:
             print("Response ist None oder ungültig")
 
-    def updateScenario(scenarioId:str) -> None:
+    def updateScenario(scenarioId: str) -> None:
         try:
-            requests.put(f"http://localhost:8090/Scenarios/update_scenario/{scenarioId}").json()
+            requests.put(
+                f"http://localhost:8090/Scenarios/update_scenario/{scenarioId}"
+            ).json()
         except:
             print("Response ist None oder ungültig")
 
     def launchScenario(scenarioId) -> None:
         try:
-            requests.post(f"http://localhost:8090/Runner/launch_scenario/{scenarioId}").json()
+            requests.post(
+                f"http://localhost:8090/Runner/launch_scenario/{scenarioId}"
+            ).json()
         except:
             print("Response ist None oder ungültig")
 
-class Backend():
-    def getCustomer(customerId:str) -> Customer:
+
+class Backend:
+    def getCustomer(customerId: str) -> Customer:
         try:
             return Customer(
                 requests.get(f"http://localhost:8080/customers/{customerId}").json()
@@ -44,30 +56,44 @@ class Backend():
         except:
             print("Response ist None oder ungültig")
 
-    def getAllCustomers(scenarioId:str) -> list[Customer]:
+    def getAllCustomers(scenarioId: str) -> list[Customer]:
         try:
-            return [Customer(c) for c in requests.get(f"http://localhost:8080/scenarios/{scenarioId}/customers").json()]
+            return [
+                Customer(c)
+                for c in requests.get(
+                    f"http://localhost:8080/scenarios/{scenarioId}/customers"
+                ).json()
+            ]
         except:
             print("Response ist None oder ungültig")
 
     # TODO: Add Metadata Response
-    def getScenarioMetadata(scenarioId:str) -> None:
+    def getScenarioMetadata(scenarioId: str) -> None:
         try:
-            return requests.get(f"http://localhost:8080/scenario/{scenarioId}/metadata").json()
+            return requests.get(
+                f"http://localhost:8080/scenario/{scenarioId}/metadata"
+            ).json()
         except:
             print("Response ist None oder ungültig")
 
-    def createScenario(numberOfVehicles:int = 5, numberOfCustomers:int = 5) -> Scenario:
+    def createScenario(
+        numberOfVehicles: int = 5, numberOfCustomers: int = 5
+    ) -> Scenario:
         try:
             return Scenario(
-                requests.post(f"http://localhost:8080/scenario/create?numberOfVehicles={numberOfVehicles}&numberOfCustomers={numberOfCustomers}").json()
+                requests.post(
+                    f"http://localhost:8080/scenario/create?numberOfVehicles={numberOfVehicles}&numberOfCustomers={numberOfCustomers}"
+                ).json()
             )
         except:
             print("Response ist None oder ungültig")
 
     def getAllScenarios() -> list[Scenario]:
         try:
-            return [Scenario(s) for s in requests.get(f"http://localhost:8080/scenarios").json()]
+            return [
+                Scenario(s)
+                for s in requests.get(f"http://localhost:8080/scenarios").json()
+            ]
         except:
             print("Response ist None oder ungültig")
 
@@ -77,7 +103,7 @@ class Backend():
         except:
             print("Response ist None oder ungültig")
 
-    def getScenario(scenarioId:str) -> Scenario:
+    def getScenario(scenarioId: str) -> Scenario:
         try:
             return Scenario(
                 requests.get(f"http://localhost:8080/scenarios/{scenarioId}").json()
@@ -85,13 +111,18 @@ class Backend():
         except:
             print("Response ist None oder ungültig")
 
-    def getAllVehicles(scenarioId:str) -> list[Vehicle]:
+    def getAllVehicles(scenarioId: str) -> list[Vehicle]:
         try:
-            return [Vehicle(v) for v in requests.get(f"http://localhost:8080/scenarios/{scenarioId}/vehicles").json()]
+            return [
+                Vehicle(v)
+                for v in requests.get(
+                    f"http://localhost:8080/scenarios/{scenarioId}/vehicles"
+                ).json()
+            ]
         except:
             print("Response ist None oder ungültig")
 
-    def getVehicle(vehicleId:str) -> Vehicle:
+    def getVehicle(vehicleId: str) -> Vehicle:
         try:
             return Vehicle(
                 requests.get(f"http://localhost:8080/vehicles/{vehicleId}").json()
---
+++ test.py
@@ -0,0 +1,6 @@
+from backend.TelcAPI import Backend, Runner
+from backend.Wrapper.Scenario import Scenario
+import json
+
+scenario: Scenario = Backend.getScenario("973dc6f2-2cde-4872-9689-ac7f968790c0")
+print(scenario.json())
