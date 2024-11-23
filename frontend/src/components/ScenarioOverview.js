19c3b4b038ec8e122aacc423b4629a88de54a6f0
Felix Wu <felix@dot9.co>
2024-11-23 20:35:45 +0100

    scenario overview

--- frontend/src/components/ScenarioOverview.js
+++ frontend/src/components/ScenarioOverview.js
@@ -71,41 +71,7 @@ function ScenarioOverview({ id }) {
     </div>
   );
 
-  return (
-    <div>
-      {scenario.vehicles.map((item) => (
-        <div
-          key={item.id}
-          class={item.isAvailable ? "vehicle available" : "vehicle occupied"}>
-          <div class="positionProperty">
-            <b>Position:</b> {item.coordX}, {item.coordY}
-          </div>
-          <div>
-            <b>isAvailable:</b> {item.isAvailable}
-          </div>
-          <div>
-            <b>vehicleSpeed:</b> {item.vehicleSpeed}
-          </div>
-          <div>
-            <b>customerId:</b> {item.customerId}
-          </div>
-          <div>
-            <b>remainingTravelTime:</b> {item.remainingTravelTime}
-          </div>
-          <div>
-            <b>distanceTravelled:</b> {item.distanceTravelled}
-          </div>
-          <div>
-            <b>activeTime:</b> {item.activeTime}
-          </div>
-          <div>
-            <b>numberOfTrips:</b> {item.numberOfTrips}
-          </div>
-          <div class="idProperty">ID: {item.id}</div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div>{renderData()}</div>;
 }
 
 export default ScenarioOverview;
